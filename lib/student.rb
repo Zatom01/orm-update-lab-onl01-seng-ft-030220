b6b80bc26cca285a67d9f40dff740a86b1e3b602
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-03 01:15:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,10 +43,10 @@ class Student
   end 
   
   def self.new_from_db(row)
-    student=Student.new 
-    student.id=row[0]
-    student.name=row[1]
-    student.grade=row[2]
+    id=row[0]
+    name=row[1]
+    grade=row[2]
+    student=Student.new(id,name,row)
   end 
 
 
